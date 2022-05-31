2b8e3eeb55b5721f14ab0b0103bfd349f0519975
nickgiggey <nickgiggey@comcast.net>
2022-05-31 09:42:43 -0600

    remove unneeded code

--- js/app.js
+++ js/app.js
@@ -63,9 +63,3 @@ function typeWriter() {
 }
 typeWriter();
 // -------------------------------------------------------Lazy
-highResImage.onload = function () {
-    lowResImage.src = highResImage.src;
-};
-setTimeout(function () {
-    highResImage.src = urlencode(highRes);
-}, 1000);
